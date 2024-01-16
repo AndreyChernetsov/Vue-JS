07e3b839e89082cbc0d509f0ecf08839d0446aef
AndreyChernetsov <andcher24@gmail.com>
2024-01-16 19:51:10 +0700

    Проверочная работа №5

--- js/main.js
+++ js/main.js
@@ -6,19 +6,35 @@ let app = new Vue({
     image: "./assets/vmSocks-green-onWhite.jpg",
     altText: "A pair of socks",
     inStock: true,
+    cart: 0,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
 
     variants: [
         {
-        variantId: 2234,
-        variantColor: 'green'
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
         },
         {
-        variantId: 2235,
-        variantColor: 'blue'
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
         }
     ],
 
-    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    updateProduct(variantImage) {
+        this.image = variantImage
+    }
+    },
+
+    methods: {
+        addToCart() {
+            cart = 0
+            this.cart += 1
+        },
+        deleteToCart() {
+            cart = 0
+            this.cart -= 1
+        }
     },
 })
