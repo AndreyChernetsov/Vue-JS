9b3a6b860d3046d95190636ecfee3871251107d9
AndreyChernetsov <andcher24@gmail.com>
2024-01-16 21:23:34 +0700

    Проверочная работа №7

--- js/main.js
+++ js/main.js
@@ -1,35 +1,54 @@
-let product = "Socks";
 let app = new Vue({
     el: '#app',
     data: {
-    product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
-    altText: "A pair of socks",
-    inStock: false,
-    cart: 0,
-    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-
-    variants: [
-        {
-            variantId: 2234,
-            variantColor: 'green',
-            variantImage: "./assets/vmSocks-green-onWhite.jpg",
-        },
-        {
-            variantId: 2235,
-            variantColor: 'blue',
-            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-        }
-    ],
-
+        product: "Socks",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        altText: "A pair of socks",
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
+            }
+        ],
+        cart: 0,
+            
     },
-
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }            
     },
-})
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+
+        sale() {
+            return this.title + " is " + this.onSale;
+        }
+    },  
+})    
